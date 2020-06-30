ce608821236df7c6d5b5e1d88a48015f5b5e81d3
yogeshpandare <yogispandare@gmail.com>
2020-06-30 10:56:19 -0400

    one new comment in com.yogesh package

--- src/com/yogesh/Test.java
+++ src/com/yogesh/Test.java
@@ -2,13 +2,13 @@ package com.yogesh;
 
 public class Test {
 
+	// local variable question
 
-		   public static void main(String args[]) {
-				/*
-				 * int i; do { i++; // local variable i may not be initialized } while(i < 0);
-				 * System.out.println(i);
-				 */
-		   }
-		}
-	
+	public static void main(String args[]) {
 
+		/*
+		 * int i; do { i++; // local variable i may not be initialized } while(i < 0);
+		 * System.out.println(i);
+		 */
+	}
+}
